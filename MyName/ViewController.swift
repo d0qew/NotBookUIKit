ba78bbd100d928a8ec95fba817cb67320bb70572
d0qew <dkonashenko7@gmail.com>
2022-09-22 14:36:33 +0300

    Initial Commit

---
+++ MyName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyName
+//
+//  Created by Daniil Konashenko on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
